d423313052dacc853f41d40e40fa24d1daf11a8e
mukulkumar2109 <154835226+mukulkumar2109@users.noreply.github.com>
2024-12-09 01:00:05 +0530

    Delete fitness_backend/ai_processing directory

--- fitness_backend/ai_processing/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.process_video, name='process_video'),
-    path('stop_video_feed', views.stop_video_feed, name='stop-video'),
-    path('live_exercise', views.live_exercise, name='live-exercise'),
-    path('video', views.video_feed, name='video_feed')
-]
